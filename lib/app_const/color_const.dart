c99c5ffcb460dbfde3dcdabd0ef5013e4d828214
Prerna <prernamittal440@gmail.com>
2024-12-01 16:07:44 +0530

    Final Commit

--- lib/app_const/color_const.dart
+++ lib/app_const/color_const.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 
 class ColorConst{
 
+  static List<Color> priorityColor = [
+    ColorConst().mColor[4],
+    ColorConst().mColor[2],
+    ColorConst().mColor[3],
+  ];
   List<Color> mColor=[
     Color.fromARGB(255, 10,175,167), //0
     Colors.lightBlue, //1
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,20 +11,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   MediaQueryData? mqData;
-
+  String? selectedPriority;
+  int? selectedPriorityNumber;
+  bool isChecked = false;
   List<TodoModel> mList = [];
-
+  List<String> priority = ["Low", "High"];
+  Color? selectedColor;
   TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
 
-  TextEditingController DateController = TextEditingController();
-
-  TextEditingController PriorityController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
 
   DateTime? selectedDate;
   String? formattedDate;
 
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
+    var collections = fireStore.collection("todos");
     mqData = MediaQuery.of(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -47,54 +52,122 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
-      body: mList.isNotEmpty
-          ? ListView.builder(
-              itemCount: mList.length,
-              itemBuilder: (_, index) {
-                return SizedBox(
-                  height: mqData!.size.height * 0.13,
-                  child: Card(
-                    child: CheckboxListTile(
-                      value: false,
-                      onChanged: (value) {},
-                    ),
-                  ),
-                );
-              })
-          : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "No Todo's Yet..",
-                    style: TextStyle(
-                        fontFamily: 'AppBar',
-                        fontWeight: FontWeight.bold,
-                        color: ColorConst().mColor[0],
-                        fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 7,
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorConst().mColor[0],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                      onPressed: () {
-                        showModalBottomSheet(
-                            context: (context),
-                            builder: (_) {
-                              return _bottomSheetContent(context: context);
-                            });
-                      },
-                      child: Text(
-                        "Add Now",
-                        style: TextStyle(color: Colors.white),
-                      ))
-                ],
-              ),
-            ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: collections.snapshots(),
+          builder: (_, snapshots) {
+            if (snapshots.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshots.hasData) {
+              return snapshots.data!.docs.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: snapshots.data!.docs.length,
+                      itemBuilder: (_, index) {
+                        return SizedBox(
+                          height: mqData!.size.height * 0.13,
+                          child: Card(
+                            child: CheckboxListTile(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              title: Text(
+                                  snapshots.data!.docs[index].data()['title']),
+                              value:
+                                  snapshots.data!.docs[index].data()['isComp'],
+                              onChanged: (bool? value) {
+                                isChecked = value!;
+                                collections
+                                    .doc(snapshots.data!.docs[index].id)
+                                    .update(TodoModel(
+                                            title: snapshots.data!.docs[index]
+                                                .data()['title'],
+                                            desc: snapshots.data!.docs[index]
+                                                .data()['desc'],
+                                            assignAt: snapshots
+                                                .data!.docs[index]
+                                                .data()['assignAt'],
+                                            isCompleted: isChecked,
+                                            completeAt: snapshots
+                                                .data!.docs[index]
+                                                .data()['completeAt'],
+                                            priority: snapshots
+                                                .data!.docs[index]
+                                                .data()['priority'])
+                                        .toMap());
+                                setState(() {});
+                              },
+
+                              tileColor: snapshots.data!.docs[index]
+                                          .data()['isComp'] ==
+                                      false
+                                  ? Colors.blue.shade200
+                                  : Colors.green.shade200,
+                              subtitle: Text(
+                                  snapshots.data!.docs[index].data()['desc']),
+                              secondary: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      "Assign At: ${snapshots.data!.docs[index].data()['assignAt']}"),
+                                  Text(
+                                      "Complete At: ${snapshots.data!.docs[index].data()['completeAt']}"),
+                                  Icon(
+                                    Icons.remove_circle,
+                                    color: snapshots.data!.docs[index]
+                                                .data()['priority'] ==
+                                            1
+                                        ? Colors.green
+                                        : Colors.red,
+                                  ),
+                                ],
+                              ),
+                              activeColor: Colors.blue,
+                              checkColor: Colors.white,
+                              controlAffinity: ListTileControlAffinity
+                                  .leading, // Position of the checkbox
+                            ),
+                          ),
+                        );
+                      })
+                  : Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "No Todo's Yet..",
+                            style: TextStyle(
+                                fontFamily: 'AppBar',
+                                fontWeight: FontWeight.bold,
+                                color: ColorConst().mColor[0],
+                                fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: ColorConst().mColor[0],
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {
+                                showModalBottomSheet(
+                                    context: (context),
+                                    builder: (_) {
+                                      return _bottomSheetContent(
+                                          context: context);
+                                    });
+                              },
+                              child: Text(
+                                "Add Now",
+                                style: TextStyle(color: Colors.white),
+                              ))
+                        ],
+                      ),
+                    );
+            }
+            return Container();
+          }),
       floatingActionButton: FloatingActionButton(
         shape: CircleBorder(),
         splashColor: ColorConst().mColor[0],
@@ -117,8 +190,8 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bottomSheetContent({required BuildContext context}) {
-    return Container(
-      height: mqData!.size.height * 0.5,
+    return SizedBox(
+      height: mqData!.size.height * 0.56,
       width: mqData!.size.width,
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10),
@@ -136,7 +209,7 @@ class _HomePageState extends State<HomePage> {
               height: 4,
             ),
             Container(
-              height: mqData!.size.height * 0.4,
+              height: mqData!.size.height * 0.46,
               width: mqData!.size.width,
               decoration: BoxDecoration(
                   border: Border.all(width: 3, color: ColorConst().mColor[0]),
@@ -153,11 +226,20 @@ class _HomePageState extends State<HomePage> {
                           hintText: "Enter your title",
                           label: Text("Title"),
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
+                              borderRadius: BorderRadius.circular(5))),
+                    ),
+                    /*---------Add Description------------*/
+                    TextField(
+                      controller: descController,
+                      decoration: InputDecoration(
+                          hintText: "Enter your Description",
+                          label: Text("Description"),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5))),
                     ),
                     /*---------DatePicker------------*/
                     TextField(
-                      controller: DateController,
+                      controller: dateController,
                       decoration: InputDecoration(
                         hintText: "Eg: Nov,23 2027",
                         label: Text("Date"),
@@ -171,31 +253,73 @@ class _HomePageState extends State<HomePage> {
                               formattedDate =
                                   DateFormat('MMM,dd yyyy').format(date);
                               if (formattedDate != null) {
-                                DateController.text = formattedDate!;
+                                dateController.text = formattedDate!;
                               }
                               setState(() {});
                             },
                             child: Icon(Icons.calendar_month)),
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
+                            borderRadius: BorderRadius.circular(5)),
                       ),
                     ),
                     /*---------Priority TextField------------*/
-                    TextField(
-                      controller: PriorityController,
-                      decoration: InputDecoration(
-                          hintText: "Enter your Priority",
-                          label: Text("Priority"),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
+                    StatefulBuilder(builder: (_, mSetState) {
+                      return DropdownMenu(
+                        hintText: "Select Priority",
+                        width: mqData!.size.width * 1,
+                        trailingIcon: Icon(
+                          Icons.remove_circle,
+                          color: selectedColor,
+                        ),
+                        dropdownMenuEntries: priority
+                            .map((item) => DropdownMenuEntry<String>(
+                                  value: item,
+                                  label: item,
+                                ))
+                            .toList(),
+                        onSelected: (value) {
+                          selectedPriority = value;
+                          mSetState(() {
+                            if (value == "Low") {
+                              selectedColor = ColorConst.priorityColor[0];
+                              selectedPriorityNumber = 1;
+                            } else if (value == "Medium") {
+                              selectedColor = ColorConst.priorityColor[1];
+                              selectedPriorityNumber = 2;
+                            } else {
+                              selectedColor = ColorConst.priorityColor[2];
+                              selectedPriorityNumber = 3;
+                            }
+                          });
+                        },
+                      );
+                    }),
                     /*---------Add Task Button------------*/
                     ElevatedButton(
                         style: ElevatedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10)),
                             backgroundColor: ColorConst().mColor[0]),
-                        onPressed: () {},
+                        onPressed: () {
+                          var date = DateTime.now();
+                          if (titleController.text.toString() != "" &&
+                              descController.text.toString() != "" &&
+                              selectedPriorityNumber != null &&
+                              dateController.text.toString() != "") {
+                            var collections = fireStore.collection("todos");
+                            collections.add(TodoModel(
+                                    title: titleController.text.toString(),
+                                    desc: descController.text.toString(),
+                                    priority: selectedPriorityNumber,
+                                    completeAt: dateController.text.toString(),
+                                    assignAt: DateFormat('MMM,dd yyyy')
+                                        .format(date)
+                                        .toString())
+                                .toMap());
+                            setState(() {});
+                            Navigator.pop(context);
+                          }
+                        },
                         child: Text(
                           "Add Now",
                           style: TextStyle(color: Colors.white),
@@ -208,5 +332,14 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  Color getBackGroudColor(int priority) {
+    if (priority == 1) {
+      return Colors.blue;
+    } else if (priority == 2) {
+      return Colors.redAccent;
+    }
+    return Colors.orange;
   }
 }
